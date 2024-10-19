9c808172c18be4250dd8b000bf4e98bb6002fe3e
Sean Sponsler <seansponsler@Seans-MacBook-Pro-10.local>
2024-10-18 18:55:03 -0700

    Removed old main and DisplayArea classes, refactored AltMain -> Main

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,82 @@
+import Controller.MainController;
+import DataClients.EmotionDataClient;
+import DataClients.EyeTrackingClient;
 import Model.Blackboard;
 import Model.DataProcessor;
 import Model.Observer;
-import View.DisplayArea;
-import javax.swing.SwingUtilities;
+import TestServers.EmotionDataServer;
+import TestServers.EyeTrackingServer;
+import View.ColorKeyPanel;
+import View.DrawPanel;
+import View.PreferencePanel;
+import java.awt.*;
+import javax.swing.*;
 
-public class Main {
-   public static void main(String[] args) {
-       Blackboard blackboard = Blackboard.getInstance();
-       DataProcessor dataProcessor = new DataProcessor();
-       Observer observer = new Observer();
-       DisplayArea displayArea = new DisplayArea();
-       
+public class Main extends JFrame {
 
-       Thread dataThread = new Thread(dataProcessor);
-       Thread observerThread = new Thread(observer);
-       //Thread displayThread = new Thread(displayArea);
+    private static final String TESTING_FLAG = "-test";
+    public static void main (String[] args){
+        Main window = new Main();
+        window.setSize(1200,1000); // center on screen
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-       //displayThread.start();
-       dataThread.start();
-       observerThread.start();
+        if (args.length > 0 && args[0].equals(TESTING_FLAG)){
+            window.startServerThreads();
+        }
+        window.startAllThreads();
+    }
+    public Main() {
+        super("Eye Tracking & Emotion Hub");
+        setLayout(new BorderLayout());
 
-//       SwingUtilities.invokeLater(blackboard.getDisplayArea());
+        JMenuBar menuBar = new JMenuBar();
+        JMenu actionsMenu = new JMenu("Actions");
+        JMenuItem start = new JMenuItem("Start");
+        JMenuItem stop = new JMenuItem("Stop");
 
-   }
+        menuBar.add(actionsMenu);
+        actionsMenu.add(start);
+        actionsMenu.add(stop);
+        setJMenuBar(menuBar);
+
+        MainController controller = new MainController();
+        start.addActionListener(controller);
+        stop.addActionListener(controller);
+
+        PreferencePanel preferencePanel = new PreferencePanel();
+        add(preferencePanel, BorderLayout.NORTH);
+
+        DrawPanel drawPanel = Blackboard.getInstance().getDrawPanel();
+        drawPanel.setPreferredSize(new Dimension(1000,1000));
+        add(drawPanel, BorderLayout.CENTER);
+
+        ColorKeyPanel colorKeyPanel = new ColorKeyPanel();
+        colorKeyPanel.setPreferredSize(new Dimension(200,1000));
+        add(colorKeyPanel, BorderLayout.EAST);
+    }
+
+    private void startAllThreads() {
+
+        Thread emotionThread = new Thread(new EmotionDataClient());
+        Thread eyeTrackingThread = new Thread(new EyeTrackingClient());
+        Thread dataThread = new Thread(new DataProcessor());
+        Thread observerThread = new Thread(new Observer());
+
+        emotionThread.start();
+        eyeTrackingThread.start();
+        dataThread.start();
+        observerThread.start();
+    }
+
+    private void startServerThreads(){
+        Thread emotionServerThread = new Thread(new EmotionDataServer());
+        Thread eyeTrackingThread = new Thread(new EyeTrackingServer());
+
+        emotionServerThread.start();
+        eyeTrackingThread.start();
+    }
+
+
 }
